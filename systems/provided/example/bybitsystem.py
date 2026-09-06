6d21b19882bd48a65a421ee9c36bbe268672167e
cheremnovkv <cheremnovkv@sar-group.ru>
2026-09-06 18:38:55 +0800

    Стартовый портфель для ByBit

---
+++ systems/provided/example/bybitsystem.py
@@ -0,0 +1,37 @@
+from sysdata.config.configdata import Config
+from sysdata.sim.bybit_futures_sim_data import bybitFuturesSimData
+
+from systems.forecasting import Rules
+from systems.basesystem import System
+from systems.forecast_combine import ForecastCombine
+from systems.forecast_scale_cap import ForecastScaleCap
+from systems.positionsizing import PositionSizing
+from systems.portfolio import Portfolios
+from systems.accounts.accounts_stage import Account
+from systems.rawdata import RawData
+
+
+def bybitsystem(data=None, config=None):
+    """
+    Example of how to 'wrap' a complete ByBit system
+    """
+    if config is None:
+        config = Config("systems.provided.example.bybitconfig.yaml")
+    if data is None:
+        data = bybitFuturesSimData()
+
+    my_system = System(
+        [
+            Account(),
+            Portfolios(),
+            PositionSizing(),
+            ForecastCombine(),
+            ForecastScaleCap(),
+            Rules(),
+            RawData(),
+        ],
+        data,
+        config,
+    )
+
+    return my_system
